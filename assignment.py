e839aca034ddf56dc3ac6b10ebe5d7f6b5d6d0d5
Mnyaxx <162140858+Mnyaxx@users.noreply.github.com>
2025-03-18 11:57:21 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,28 @@
+def calculator():
+    print("Welcome to the simple calculator!")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    op = input("What do you want to do? (+, -, *, /): ")
+    
+    if op == '+':
+        result = num1 + num2
+        print("The answer is:", result)
+    elif op == '-':
+        result = num1 - num2
+        print("The answer is:", result)
+    elif op == '*':
+        result = num1 * num2
+        print("The answer is:", result)
+    elif op == '/':
+        if num2 == 0:
+            print("Oops! You can't divide by zero.")
+        else:
+            result = num1 / num2
+            print("The answer is:", result)
+    else:
+        print("Sorry, that's not a valid operation.")
+    
+    print("Thanks for using the calculator!")
+
+calculator()
 
